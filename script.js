7343d83ce2b3b42afa54f117f806bb98823c25f9
Luke Carbis <luke@carb.is>
2023-09-20 17:16:04 +1000

    Fix results loading bug

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const slug = params.get('slug');
 
     if (slug) {
-        const results = document.getElementById('results');
-        results.innerHTML = "<p>Loading...</p>";
+        const resultsDiv = document.createElement("div");
+        resultsDiv.id = "results";
+        document.body.insertBefore(resultsDiv, document.body.firstChild);
+
+        resultsDiv.innerHTML = "<p>Loading...</p>";
 
         fetchData(slug)
             .then(data => {
-                const resultsDiv = document.createElement("div");
-                resultsDiv.id = "results";
                 resultsDiv.innerHTML = `
                     <h2><a href="https://wordpress.org/plugins/${slug}" target="_blank">${data.name}</a></h2>
                     <ul>
@@ -31,10 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     </ul>
                 `;
 
-                document.body.insertBefore(resultsDiv, document.body.firstChild);
             })
             .catch(error => {
-                results.innerHTML = `<p>Couldn't find <code>${slug}</code>.</p>`;
+                resultsDiv.innerHTML = `<p>Couldn't find <code>${slug}</code>.</p>`;
             });
     }
 });
