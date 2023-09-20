b21548505c62d95a7e482bca17c7f83b1c2eebe6
Luke Carbis <luke@carb.is>
2023-09-20 12:54:33 +1000

    Emphasize estimate

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ document.getElementById('pluginForm').addEventListener('submit', function(event)
                 <li><a href="https://wordpress.org/plugins/${slug}" target="_blank">${data.name}</a></li>
                 <li>Last Updated: ${data.lastUpdated}</li>
                 <li>Last Peak: ${data.normalizedDownloads.latestPeakValue.toLocaleString()}</li>
+                <li>Installs Since Last Peak: ${data.normalizedDownloads.sumAfterPeak.toLocaleString()}</li>
                 <li>Last Version Percentage: ${data.latestVersionPercentage}%</li>
-                <li>Estimated Active Installs: ${data.estimatedInstalls.toLocaleString()}</li>
                 <li>Reported Active Installs: ${data.reportedInstalls.toLocaleString()}+</li>
+                <li><strong>Estimated Active Installs: ${data.estimatedInstalls.toLocaleString()}</strong></li>
             `;
 
         })
